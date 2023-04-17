115002416e095b685ce737e5ff79859c81a92988
JETTPLAYER <130397144+JETTPLAYER@users.noreply.github.com>
2023-04-17 12:39:38 -0600

    Add files via upload

---
+++ script/funciones.js
@@ -0,0 +1,3 @@
+function cargaPractica(htmlDocument) {
+	document.getElementById("content").innerHTML = '<object width=\"100%\" height=\"100%\" type="text/html" data="' + htmlDocument + '" ></object>';
+}
