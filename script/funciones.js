9a403efb096dacd0d9fe712db71ac318fd821b42
JETTPLAYER <130397144+JETTPLAYER@users.noreply.github.com>
2023-04-17 13:05:26 -0600

    Delete script directory

--- script/funciones.js
+++
@@ -1,3 +0,0 @@
-function cargaPractica(htmlDocument) {
-	document.getElementById("content").innerHTML = '<object width=\"100%\" height=\"100%\" type="text/html" data="' + htmlDocument + '" ></object>';
-}
